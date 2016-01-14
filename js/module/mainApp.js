f85cfdd4f19004b15beb542d8152bcfdcbedcded
dazhezi <727236692@qq.com>
2016-01-14 16:40:21 +0800

    提交学习项目

---
+++ js/module/mainApp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by zhangz on 2016/1/12.
+ */
+var mainApp = angular.module("mainApp", []);
---
+++ js/module/studentController.js
@@ -0,0 +1,22 @@
+/**
+ * Created by zhangz on 2016/1/12.
+ */
+mainApp.controller("studentController", function($scope) {
+    $scope.student = {
+        firstName: "周",
+        lastName: "杰伦",
+        fees:500,
+        subjects:[
+            {name:'物理',marks:78},
+            {name:'化学',marks:82},
+            {name:'语文',marks:68},
+            {name:'外语',marks:79},
+            {name:'Java',marks:87}
+        ],
+        fullName: function() {
+            var studentObject;
+            studentObject = $scope.student;
+            return studentObject.firstName + " " + studentObject.lastName;
+        }
+    };
+});
